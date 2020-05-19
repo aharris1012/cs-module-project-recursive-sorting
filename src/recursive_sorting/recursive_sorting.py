d2ec47513e57ad9ed3d9ad49bfe0f6b0f6e3c76b
Andrea Harris <andrea@andreas-imac.lan>
2020-05-19 16:29:07 -0500

    merge

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,7 +4,23 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    next_smallest_left_index = 0
+    next_smallest_right_index = 0
+    
+    for i in range (elements):
+        if next_smallest_left_index >= len(arrA):
+            return merged_arr.extend(arrB[next_smallest_right_index:])
+        
+        elif next_smallest_right_index >= len (arrB):
+            return merged_arr.extend(arrA[next_smallest_left_index:])
 
+        elif arrA[next_smallest_left_index] < arrB[next_smallest_right_index]:
+            merged_arr[i] = arrA[next_smallest_left_index]
+            next_smallest_left_index += 1
+
+        else:
+            merged_arr[i] =arrB[next_smallest_right_index]
+            next_smallest_right_index +=1
 
     return merged_arr
 
